3e9247ffb4e1e145f259bcadc826da07755e4642
MD-MOKSHIDUL-MOMIN <139842805+MD-MOKSHIDUL-MOMIN@users.noreply.github.com>
2025-06-27 02:34:15 +0600

    code

---
+++ SEGMENT_DESPLAY_1.ino
@@ -0,0 +1,137 @@
+// USING 7 SEGMENT DESPLAY ANODE
+
+int a=7,b=6,c=5,d=4,e=3,f=2,g=1;
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+
+}
+
+void loop() {
+  // DESPLAY - 0
+
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+  
+  delay(1000);
+
+  // DESPLAY - 1
+
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+  
+  delay(1000);
+
+  // DESPLAY - 2
+
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  // DESPLAY - 3
+
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  // DESPLAY - 4
+
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  // DESPLAY - 5
+
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  // DESPLAY - 6
+
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  // DESPLAY - 7
+
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+  
+  delay(1000);
+
+  // DESPLAY - 8
+
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  // DESPLAY - 5
+
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  
+  delay(1000);
+
+  
+}
